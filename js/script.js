02d0a00f73d05a90a75217d853a0b94c862d3a3a
ddey123 <debayan1206@gmail.com>
2020-10-29 18:26:00 +0000

    Commited files

---
+++ js/script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    function realtimeClock() {
+        var rtClock = new Date();
+
+        var hours = rtClock.getHours();
+        var minutes = rtClock.getMinutes();
+        var seconds = rtClock.getSeconds();
+
+        var amPm = (hours < 12) ? "AM" : "PM";
+
+        hours = (hours > 12) ? hours - 12 : hours;
+
+        hours = ("0" + hours).slice(-2);
+        minutes = ("0" + minutes).slice(-2);
+        seconds = ("0" + seconds).slice(-2);
+
+        document.getElementById('clock').innerHTML = 
+            hours + "  :  " + minutes + "  :  " + seconds + " " + amPm;
+        var t = setTimeout(realtimeClock, 500);
+        }
+    realtimeClock();
+});
