adf78d86419ddcc486b0fccf0ca39c4a272ade40
ddey123 <debayan1206@gmail.com>
2020-10-30 11:18:11 +0000

    Added a date to extension

--- js/script.js
+++ js/script.js
@@ -18,5 +18,23 @@ document.addEventListener('DOMContentLoaded', () => {
             hours + "  :  " + minutes + "  :  " + seconds + " " + amPm;
         var t = setTimeout(realtimeClock, 500);
         }
+    function realTimeDate() {
+        var d = new Date();
+        var date = d.getDate();
+        var month = d.getMonth() + 1;
+        var FullYear = d.getFullYear();
+        var weekday = new Array(7);
+        weekday[0] = "Sunday";
+        weekday[1] = "Monday";
+        weekday[2] = "Tuesday";
+        weekday[3] = "Wednesday";
+        weekday[4] = "Thursday";
+        weekday[5] = "Friday";
+        weekday[6] = "Saturday";
+        var n = weekday[d.getDay()]
+        document.getElementById("date").innerHTML = n + "  " + date + "." + month + "." + FullYear;
+    }
     realtimeClock();
+    realTimeDate();
+    
 });
